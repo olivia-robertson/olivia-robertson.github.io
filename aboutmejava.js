db7cd00b16379c51320af50933400d98140f68fd
eyevent <eyevent@USSEAGirls20.ey.net>
2018-07-25 14:01:04 -0700

    first commit

---
+++ aboutmejava.js
@@ -0,0 +1,5 @@
+function replace(){
+  var justinbieber = "http://oceanup.com/wp-content/uploads/2016/04/Justin-Bieber-choked-1.jpeg";
+  var eye = document.getElementById("eye");
+  eye.src = justinbieber;
+}
